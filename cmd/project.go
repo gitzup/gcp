e5e111597930d35a0b0dbe075139450f99d09ea8
Eric Kfir <eric@kfirs.com>
2018-09-12 23:47:16 +0300

    Draft "project init" implementation

--- cmd/project.go
+++ cmd/project.go
@@ -1,16 +1,64 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/gitzup/agent/pkg"
+	"github.com/gitzup/gcp/internal"
 	"github.com/spf13/cobra"
 )
 
-// projectCmd represents the project command
 var projectCmd = &cobra.Command{
 	Use:   "project",
 	Short: "Applies GCP project resources.",
-	Long:  `This command allows managing GCP projects.`,
+}
+
+var projectInitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initializes a GCP project resource.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		projectSchemaBytes, err := internal.Asset("schema/project.json")
+		if err != nil {
+			panic(err)
+		}
+
+		var projectSchema interface{}
+		err = json.Unmarshal(projectSchemaBytes, &projectSchema)
+		if err != nil {
+			panic(err)
+		}
+
+		response := pkg.ResourceInitResponse{
+			ConfigSchema: projectSchema,
+			DiscoveryAction: pkg.Action{
+				Name:       "discover",
+				Image:      "gitzup/gcp",
+				Entrypoint: []string{"/app/agent"},
+				Cmd:        []string{"discover"},
+			},
+		}
+		json, err := json.Marshal(&response)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.MkdirAll("/gitzup", 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		ioutil.WriteFile("/gitzup/result.json", json, 0644)
+		fmt.Println("project called")
+	},
+}
+
+var projectDiscoverCmd = &cobra.Command{
+	Use:   "discover",
+	Short: "Discovers the state of the GCP project.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("project called")
 	},
@@ -18,14 +66,6 @@ var projectCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(projectCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// projectCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// projectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	projectCmd.AddCommand(projectInitCmd)
+	projectCmd.AddCommand(projectDiscoverCmd)
 }
